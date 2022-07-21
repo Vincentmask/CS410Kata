26441d7c6195e8ae2aa16c11157205591b7abc6c
Vincentmask <liangmask@gmail.com>
2022-07-20 21:04:19 -0700

    Mob Programming 7/20, RPN Calculator

--- Kata/src/it/java/edu/pdx/cs410J/LV/KataIT.java
+++ Kata/src/it/java/edu/pdx/cs410J/LV/KataIT.java
@@ -9,10 +9,38 @@ import static org.hamcrest.core.StringContains.containsString;
 class KataIT extends InvokeMainTestCase {
 
   @Test
-  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "5");
-    assertThat(result.getTextWrittenToStandardError(), containsString("Foo"));
+  void test1() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "20", "5", "/");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("The result is: 4"));
   }
 
+  @Test
+  void test2() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "4", "2", "+", "3", "-");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("The result is: 3"));
+  }
+
+  @Test
+  void test3() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "3", "5", "8", "*", "7", "+", "*");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("The result is: 141"));
+  }
+
+  @Test
+  void test4() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "9", "SQRT");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("The result is: 3"));
+  }
+
+  @Test
+  void test5() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "5","3", "4", "2", "9", "1", "MAX");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("The result is: 9"));
+  }
+
+  void test6 () {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "4","5", "MAX", "1", "2", "MAX", "*");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("The result is: 10"));
+  }
 
 }
